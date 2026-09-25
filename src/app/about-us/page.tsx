53017fe776b7de37ae85e82b2e0d0836afbf3108
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2026-09-25 22:23:35 +0700

    edit about us

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,6 +3,7 @@ import masIman from "@/components/images/Iman.jpg";
 import masSatya from "@/components/images/Satya.jpg";
 import masRezi from "@/components/images/Rezi.jpg";
 import masHaikal from "@/components/images/Haikal.jpeg";
+import SectionTitle from "@/components/SectionTitle";
 
 const page = () => {
   return (
@@ -16,93 +17,98 @@ const page = () => {
         
       </div> */}
 
-      <div className="flex flex-col justify-center items-center gap-8 py-8 rounded-2xl">
-        <h1 className="text-4xl font-extrabold text-emerald-900 pb-6">
-          Our Team
-        </h1>
-        <div className="flex gap-8 px-8">
-          <div className="w-2/5">
-            <Image
-              className="rounded-3xl"
-              src={masIman.src}
-              alt={""}
-              width={800}
-              height={1200}
-            />
+      <div className="flex flex-col justify-center items-center gap-8 mt-4 mb-20 rounded-2xl">
+        <SectionTitle>Our Team</SectionTitle>
+        <main className="grid grid-cols-2 gap-16">
+          <div className="flex gap-8 px-8">
+            <div className="w-2/5">
+              <Image
+                className="rounded-3xl"
+                src={masIman.src}
+                alt={""}
+                width={800}
+                height={1200}
+              />
+            </div>
+            <div className="w-3/5 flex flex-col justify-center">
+              <h2 className="text-6xl font-extrabold text-indigo-900">
+                Iman Finuaz
+              </h2>
+              <p className="text-2xl text-indigo-950 font-bold pt-4">
+                Project Manager & BE
+              </p>
+            </div>
           </div>
-          <div className="w-3/5 flex flex-col justify-center items-center">
-            <h2 className="text-6xl font-extrabold text-indigo-900">
-              Iman Finuaz
-            </h2>
-            <p className="text-2xl text-indigo-950 font-bold pt-4">
-              Project Manager & BE
-            </p>
+          <div className="flex w-full justify-between gap-8 px-8">
+            <div className="w-3/5 flex flex-col justify-center">
+              <h2 className="text-6xl font-extrabold text-red-700 text-right">
+                Rezi Rama
+              </h2>
+              <p className="text-2xl text-red-800 font-bold pt-4 text-right">
+                Content & Research
+              </p>
+            </div>
+            <div className="w-2/5">
+              <Image
+                className="rounded-3xl"
+                src={masRezi.src}
+                alt={""}
+                width={800}
+                height={1200}
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex w-full justify-between gap-8 px-8">
-          <div className="w-3/5 flex flex-col justify-center items-center">
-            <h2 className="text-6xl font-extrabold text-red-700">Rezi Rama</h2>
-            <p className="text-2xl text-red-800 font-bold pt-4">
-              Content & Research
-            </p>
+          <div className="flex gap-8 px-8">
+            <div className="w-2/5 flex items-center">
+              <Image
+                className="rounded-3xl"
+                src={masSatya.src}
+                alt={""}
+                width={800}
+                height={1200}
+              />
+            </div>
+            <div className="w-3/5 flex flex-col justify-center">
+              <h2 className="text-6xl font-extrabold text-slate-800">
+                Amanda Satya
+              </h2>
+              <p className="text-2xl text-slate-900 font-bold pt-4">
+                Team Lead & FE
+              </p>
+            </div>
           </div>
-          <div className="w-2/5">
-            <Image
-              className="rounded-3xl"
-              src={masRezi.src}
-              alt={""}
-              width={800}
-              height={1200}
-            />
+          <div className="flex w-full justify-between gap-8 px-8">
+            <div className="w-3/5 flex flex-col justify-center">
+              <h2 className="text-6xl font-extrabold text-amber-800 pt-28 text-right">
+                Haikal Bintang
+              </h2>
+              <p className="text-2xl text-amber-900 font-bold pt-4 text-right">
+                FS & Design
+              </p>
+              <p className="text-3xl mt-12 text-amber-600 font-bold pt-4 text-right">
+                Continued by me
+              </p>
+            </div>
+            <div className="w-2/5">
+              <Image
+                className="rounded-3xl"
+                src={masHaikal.src}
+                alt={""}
+                width={800}
+                height={1200}
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex gap-8 px-8">
-          <div className="w-2/5">
-            <Image
-              className="rounded-3xl"
-              src={masSatya.src}
-              alt={""}
-              width={800}
-              height={1200}
-            />
-          </div>
-          <div className="w-3/5 flex flex-col justify-center items-center">
-            <h2 className="text-6xl font-extrabold text-slate-800">
-              Amanda Satya
-            </h2>
-            <p className="text-2xl text-slate-900 font-bold pt-4">
-              Team Lead & FE
-            </p>
-          </div>
-        </div>
-        <div className="flex w-full justify-between gap-8 px-8">
-          <div className="w-3/5 flex flex-col justify-center items-center">
-            <h2 className="text-6xl font-extrabold text-amber-800">
-              Haikal Bintang
-            </h2>
-            <p className="text-2xl text-amber-900 font-bold pt-4">
-              FE & Design
-            </p>
-          </div>
-          <div className="w-2/5">
-            <Image
-              className="rounded-3xl"
-              src={masHaikal.src}
-              alt={""}
-              width={800}
-              height={1200}
-            />
-          </div>
-        </div>
+        </main>
       </div>
-      <div className="bg-orange-300 p-3 my-2">
+      {/* <div className="bg-orange-300 p-3 my-2">
         <h2>Our Mission</h2>
         <p>
           It is not just to promote food as a culture, but to measure the
           nutrients and to change the perspective of healthy foods from boring
           to be super fun!
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
